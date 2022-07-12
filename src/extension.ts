deea1eb1d3ba7d404cbe57ad97cc406a6659028d
Niclas Olofsson <n@niclasolofsson.se>
2022-07-12 14:12:38 +0200

    Tidy up the code a bit and add command for copying to clipboard.

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as math from "mathjs";
 
-let myStatusBarItem: vscode.StatusBarItem;
+let statusBarItem: vscode.StatusBarItem;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  myStatusBarItem = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Right,
-    -100
-  );
-  myStatusBarItem.command = "vscode-calc-selection";
-  context.subscriptions.push(myStatusBarItem);
-
-  // register some listener that make sure the status bar
-  // item always up-to-date
-  context.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor(updateStatusBar)
-  );
-  context.subscriptions.push(
-    vscode.window.onDidChangeTextEditorSelection(updateStatusBar)
-  );
-
-  // update status bar item once at start
-  updateStatusBar();
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "vscode-calc-selection.calculateAndAddComment",
-      calculateAndAddComment
-    )
-  );
-}
-
-function calculateAndAddComment() {
+function calculateAndReplace() {
   const result = evaluateResult();
   const editor = vscode.window.activeTextEditor;
   if (!editor || result === undefined) {
@@ -48,23 +13,44 @@ function calculateAndAddComment() {
     return;
   }
   const originalText = editor.document.getText(editor.selection);
+  const commentChars = getCommentChars(editor.document.languageId);
 
   editor.edit((textEditorEdit) => {
-    textEditorEdit.replace(editor.selection, `${result} # = ${originalText}`);
+    textEditorEdit.replace(
+      editor.selection,
+      `${result} ${commentChars} = ${originalText}`
+    );
   });
+}
+
+function calculateAndCopy() {
+  const result = evaluateResult();
+  if (result === undefined) {
+    return;
+  }
+  vscode.env.clipboard.writeText(result.toString());
 }
 
 function updateStatusBar() {
   const result = evaluateResult();
   if (result === undefined) {
-    myStatusBarItem.hide();
+    statusBarItem.hide();
   } else {
-    myStatusBarItem.text = `= ${result}`;
-    myStatusBarItem.show();
+    statusBarItem.text = `= ${result}`;
+    statusBarItem.show();
   }
 }
 
-function evaluateResult() {
+function getCommentChars(languageId: string) {
+  // Ideally this info should be retrieved from the vscode language config, but there is no API.
+  // https://github.com/microsoft/vscode/issues/109919
+  if (languageId === "python") {
+    return "#";
+  }
+  return "//";
+}
+
+function evaluateResult(): number | undefined {
   const editor = vscode.window.activeTextEditor;
   if (!editor) {
     return;
@@ -76,6 +62,8 @@ function evaluateResult() {
   ) {
     return;
   }
+
+  // Trim away any garbage and allow using comma as decimal separator.
   const text = editor.document
     .getText(editor.selection)
     .trim()
@@ -90,5 +78,35 @@ function evaluateResult() {
   }
 }
 
-// this method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    -100
+  );
+  statusBarItem.command = "vscode-calc-selection.calculateAndCopy";
+  context.subscriptions.push(statusBarItem);
+
+  // Subscribe to update status bar item when changing active editor or selection.
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBar)
+  );
+  context.subscriptions.push(
+    vscode.window.onDidChangeTextEditorSelection(updateStatusBar)
+  );
+
+  // Update status bar item once at start.
+  updateStatusBar();
+
+  // Add custom commands. Remember that these must be specified in package.json as well.
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "vscode-calc-selection.calculateAndReplace",
+      calculateAndReplace
+    ),
+    vscode.commands.registerCommand(
+      "vscode-calc-selection.calculateAndCopy",
+      calculateAndCopy
+    )
+  );
+}
 export function deactivate() {}
